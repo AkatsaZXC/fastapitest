d75e2e61ec179e1c8936e7f7c03a8d2df9fea69c
Leonid <akatsaaaaa@gmail.com>
2021-02-19 13:07:46 +0300

    models update

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from enum import Enum
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "The current user"}
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+@app.get("/models/{models_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
